08d3676d261d2e16ae3bd4d8333af6bad9431df7
tlaf0504 <tlaf0504@aba19c49-6901-0010-afbb-afac277447bb>
2019-04-26 07:21:37 +0000

    Calculation of element-matrix and right-side vector of a electrostatic problem using a second-order triangle finite element implemented.
    All functions now implemented as static member of the corresponding class (as discussed on 24.04.2019).
    
    Added example of element-matrix and right-side vector calculation
    
    git-svn-id: https://svn.tugraz.at/svn/BAKK-Arbeit@4 aba19c49-6901-0010-afbb-afac277447bb

--- entities/GaussianQuadrature.m
+++ entities/GaussianQuadrature.m
@@ -2,12 +2,25 @@ classdef GaussianQuadrature
     
     properties(Constant)
         
-        % Three-point and seven-point integration is implemented
-        possible_numbers_of_integration_points = [3,7];
+        % Three-point, seven-point and fiveteen-point integration for
+        % normalized triangular regions (fixed interval limits of [0,1]) 
+        % and line integration with one to six points for arbitrary limits 
+        % are implemented.
         
-        % Coordinates and weights of integration points for 7 point
+        possible_numbers_of_triangle_integration_points = [3,7,15];
+        possible_numbers_of_line_integration_points = 1:6;
+        
+        
+        
+        % ============ Constants for triangle integration =================
+        
+        % Coordinates and weights of integration points for 7 and 3 point
         % gaussian quadrature of a trangle region with normalized
-        % corner-coordinates
+        % corner-coordinates. (Triangle corners habe coordinated of (0,0),
+        % (0,1) and (1,0)
+        %
+        % Due to the normalization the integraion has fixes limits of [0,1]
+        % in both directions.
         %
         % Table was taken from K.-J.Bathe "Finite Elemente Methoden", 2nd
         % edition, Springer 2002; S.547
@@ -57,6 +70,49 @@ classdef GaussianQuadrature
                       0.1323941527885;
                       0.225];
                  
+                  
+                  
+        % ============== Constants for line integration ===================
+        
+        % Coordinates and weights of integration points for 1- to 6-point
+        % gaussian quadrature of a line with arbitrary integraion limits
+        %
+        % Table was taken from K.-J.Bathe "Finite Elemente Methoden", 2nd
+        % edition, Springer 2002; S.542
+        % 
+        
+        % One-point integration
+        r_line_1_point = [0];
+        w_line_1_point = [2];
+        
+        % Two-point integration
+        r_line_2_point = [0.577350269189626; -0.577350269189626];
+        w_line_2_point = [1;1];
+        
+        % Three-point integration
+        r_line_3_point = [0.774596669241483; 0; -0.774596669241483];
+        w_line_3_point = [5/9; 8/9; 5/9];
+        
+        % Four-point integration
+        r_line_4_point = [0.861136311594053; 0.339981043584856; ...
+            -0.339981043584856; -0.861136311594053];
+        w_line_4_point = [0.347854845137454; 0.652145154862546; ...
+            0.652145154862546; 0.347854845137454];
+        
+        % Five-point integration
+        r_line_5_point = [0.906179845938664; 0.538469310105683; ...
+            0; -0.538469310105683; -0.906179845938664];
+        w_line_5_point = [0.236926885056189; 0.478628670499366; ...
+            128/225; 0.478628670499366; 0.236926885056189];
+        
+        % Six-point integration
+        r_line_6_point = [0.932469514203152; 0.661209386466265; ...
+            0.238619186083197; -0.238619186083197; -0.661209386466265; ...
+            -0.932469514203152];
+        w_line_6_point = [0.171324492379170; 0.360761573048139; ...
+            0.467913934572691; 0.467913934572691; 0.360761573048139; ...
+            0.171324492379170];
+        
                  
     end
     
@@ -66,7 +122,15 @@ classdef GaussianQuadrature
         function val = integrate_2D_normalized_triangle_region(...
                 integrant, number_of_integration_points)
             
-            assert(ismember(number_of_integration_points, [3,7]));
+            N_allowed = ...
+                GaussianQuadrature. ...
+                possible_numbers_of_triangle_integration_points;
+            
+%             assert(ismember(number_of_integration_points, N_allowed), ...
+%                 ['Assertion failed.\nNumber of integration points must ' ...
+%                 'be one of the following values: %s\n', ...
+%                 'You entered %d.'], ...
+%                 sprintf('%d ', N_allowed), number_of_integration_points);
             
             N = number_of_integration_points;
             if N == 7
@@ -81,12 +145,80 @@ classdef GaussianQuadrature
                 
             end
             
-            val = 0;
-            for k = 1 : N
-                val = val + 1/2 * w_vec(k) * ...
-                    integrant(zeta_vec(k), eta_vec(k));
+            val = 1/2 * (integrant(zeta_vec', eta_vec') * w_vec);     
+        end
+        
+        
+        
+        function val = integrate_1d_line(integrant, ...
+                number_of_integration_points, upper_limit, lower_limit)
+            
+            N_allowed = ...
+                GaussianQuadrature. ...
+                possible_numbers_of_line_integration_points;
+            
+            assert(ismember(number_of_integration_points, N_allowed), ...
+                ['Assertion failed.\nNumber of integration points must ' ...
+                'be one of the following values: %s\n', ...
+                'You entered %d.'], ...
+                sprintf('%d ', N_allowed), number_of_integration_points);
+            
+            
+            % Coordinates and weights are specified for a nomalized
+            % integration interval of [-1, +1].
+            % For arbitray limits the corrdinates and weights must be
+            % transformed to the input limits.
+            %
+            % According to K.-J.Bathe "Finite Elemente Methoden", 2nd
+            % edition, Springer 2002; S.541 the the transformation can be 
+            % archieved as following:
+            %
+            % r_trans = (a+b)/2 + (b-a)/2 * r_i
+            % w_trans = (b-a)/2 * w_i
+            %
+            % where r_i is the coordinate and w_i is the weight in the
+            % normalized interval, r_trans and w_trans are the transformed
+            % parameters in the arbitrary interval and 'a' and 'b' are the
+            % limits of the arbitrary interval.
+            %
+            coordinate_offset = (upper_limit + lower_limit) / 2;
+            coordinate_scaler = (upper_limit - lower_limit) / 2;
+            weight_scaler = coordinate_scaler;
+            
+            
+            switch number_of_integration_points
+                case 1
+                    r_vec = GaussianQuadrature.r_line_1_point;
+                    w_vec = GaussianQuadrature.w_line_1_point;
+                    
+                case 2
+                    r_vec = GaussianQuadrature.r_line_2_point;
+                    w_vec = GaussianQuadrature.w_line_2_point;
+                    
+                    
+                case 3
+                    r_vec = GaussianQuadrature.r_line_3_point;
+                    w_vec = GaussianQuadrature.w_line_3_point;
+                    
+                    
+                case 4
+                    r_vec = GaussianQuadrature.r_line_4_point;
+                    w_vec = GaussianQuadrature.w_line_4_point;
+                    
+                case 5
+                    r_vec = GaussianQuadrature.r_line_5_point;
+                    w_vec = GaussianQuadrature.w_line_5_point;
+                    
+                case 6
+                    r_vec = GaussianQuadrature.r_line_6_point;
+                    w_vec = GaussianQuadrature.w_line_6_point;
             end
             
+            coordinates = r_vec * coordinate_scaler + coordinate_offset;
+            coordinates = coordinates(:); % Make column vector
+            weights = w_vec * weight_scaler;
+            
+            val = integrant(coordinates') * weights;
         end
         
     end
--- main.m
+++ main.m
@@ -4,8 +4,77 @@ clc
 
 addpath('entities')
 
-elem = SecondOrderTriangleElement([0,1/2,1,1/2,0,0], [0,0,0,1/2,1,1/2]);
-fun = @(zeta, eta) ElectrostaticProblem.element_matrix_integrant(...
-        elem, 1 , 2, zeta, eta, 0.00001, 0.0005);
-val = GaussianQuadrature.integrate_2D_normalized_triangle_region(fun,7)
+a = [0,1/2,1];
+b = [0,1/2,1];
 
+xe = [0,1/2,1,1/2,0,0];
+ye = [0,0,0,1/2,1,1/2];
+% Calculation of one element of element matrix
+%
+% Compute element k_1_2 of element hatrix using epsilon_x = epsilon_y = 1
+fun = @(zeta, eta) ElectrostaticProblem.element_matrix_integrant(...
+        SecondOrderTriangleElement, xe, ye, 1 , 1, zeta, eta, 1, 1);
+    
+fprintf('Element k_12 of element matrix:\nk_12 = %f\n\n', ...
+    GaussianQuadrature.integrate_2D_normalized_triangle_region(fun,7))
+
+% Calculation of one element neumann-element of the right-side vector
+% 
+% Compute line integral of shape function 5 on side 3 with sigma = 1
+fun = @(t) ElectrostaticProblem.right_side_neumann_integrant(...
+    SecondOrderTriangleElement, xe, ye, 5, t, 3, 1);
+
+fprintf('Neuman part or right-side vector element 5:\n%f\n\n', ...
+    GaussianQuadrature.integrate_1d_line(fun, 5, 1, 0))
+
+%arrayfun(@(a) GaussianQuadrature.integrate_1d_line(fun, 5, 1, 0), 0:100000);
+
+% Calculation of one plane-element of right-side vector
+%
+% Compute plane integral of second node shape function
+fun = @(zeta, eta) ElectrostaticProblem.right_side_plane_integrant(...
+    SecondOrderTriangleElement, xe, ye, 2, zeta, eta, 1);
+
+fprintf('Plane part or right-side vector element 5:\n%f\n\n', ...
+    GaussianQuadrature.integrate_2D_normalized_triangle_region(fun,7))
+
+
+
+% Compute complete equation system for one element
+fprintf('======= Calculation of element-matrix and right-side vector for one element =========\n\n')
+A = ones(6,6);
+r = ones(6,1);
+
+eps = 8.854178*10^(-12);
+sigma = 1
+roh = 1;
+
+fprintf('Elapsed time for calculation of one element:\n')
+tic
+for k = 1 : 6
+    for l = 1 : 6
+        fun = @(zeta, eta) ElectrostaticProblem.element_matrix_integrant(...
+            SecondOrderTriangleElement, xe, ye, k , l, zeta, eta, eps, eps);
+        A(k,l) = GaussianQuadrature.integrate_2D_normalized_triangle_region(fun,7);
+    end
+    
+    fun = @(zeta, eta) ElectrostaticProblem.right_side_plane_integrant(...
+        SecondOrderTriangleElement, xe, ye, k, zeta, eta, roh);
+    
+    r(k) = GaussianQuadrature.integrate_2D_normalized_triangle_region(fun,7);
+    
+    % Assumung neumann-condition on triangle side 3
+    fun = @(t) ElectrostaticProblem.right_side_neumann_integrant(...
+    SecondOrderTriangleElement, xe, ye, k, t, 3, sigma);
+
+    r(k) = r(k) + GaussianQuadrature.integrate_1d_line(fun, k, 1, 0);
+    
+end
+toc
+fprintf('\n')
+fprintf('Element-matrix A is:\n')
+disp(A)
+
+fprintf('Right-side vector r is :\n')
+disp(r)
+        
